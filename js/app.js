2815a8970da40f56de0a2da42444911cc20918e0
SarahWinchester <indira_ruli@hotmail.com>
2018-08-21 11:41:14 -0500

    All sites added and generation in dropdown

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ console.log(data);
 
 var obtenerSedes = function (dataObj) {
     var select = document.getElementById("sedes");
-
+    
     var dataKey = Object.keys(dataObj)
-
+    
     console.log(dataKey, dataKey.length);
-
-    for (let i =0; i < dataKey.length;i++){
+    
+    for (var i =0; i < dataKey.length;i++){
         var option = document.createElement("option");
         option.setAttribute("value", dataKey[i]);
         option.innerHTML = dataKey[i];
@@ -17,10 +17,23 @@ var obtenerSedes = function (dataObj) {
 }
 var obtenerGeneracion = function(e){
     var sedes = e.target.value;
-
-    console.log(data[sedes]);
+    // console.log(data[sedes]);
+    var dataSedes= Object.keys(data[sedes])
+    generationList.innerHTML = "";
+    for(var g=0; g < dataSedes.length; g++){
+        console.log(dataSedes[g])
+        // generation option
+        var eachOptionGeneration = document.createElement("option");
+        eachOptionGeneration.setAttribute("class","each-option");
+        eachOptionGeneration.setAttribute("value",dataSedes[g]);
+        eachOptionGeneration.innerHTML= dataSedes[g];
+        generationList.appendChild(eachOptionGeneration);
+        }
 }
+//llamada a select de generacion
+var generationList = document.getElementById("generationList");
+//Llamada a sedes 
 var sedes = document.getElementById("sedes")
 sedes.addEventListener("change", obtenerGeneracion);
-
+//Llamada de  a las geneneraciones individuales 
 obtenerSedes(data);
